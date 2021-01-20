fa362a9253f531675893e2380e8ba506924534c2
Malek TRABELSI <malek.isims88@gmail.com>
2021-01-21 00:32:19 +0100

    Add coordinator and view controller for photos screen

---
+++ PhotosGallery/Presentation/Secondary-Details/PhotosCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  PhotosCoordinator.swift
+//  PhotosGallery
+//
+//  Created by Malek TRABELSI on 20/01/2021.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    
+    
+    let presentingViewController: UISplitViewController
+    var viewController: UIViewController?
+    
+    init(presentingViewController: UISplitViewController) {
+        self.presentingViewController = presentingViewController
+        
+    }
+    func start() {
+        let photosViewController = PhotosViewController()
+        self.viewController = photosViewController
+        presentingViewController.setViewController(photosViewController, for: .secondary)
+        presentingViewController.delegate = photosViewController
+    }
+}
